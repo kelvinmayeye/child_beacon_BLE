2da7a918d0a6514ac92f8009999fc60b5f1c4090
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-12 09:39:32 +0300

    working on main_activity

--- app/src/main/java/com/example/childbeaconapp/MainActivity.java
+++ app/src/main/java/com/example/childbeaconapp/MainActivity.java
@@ -13,33 +13,50 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 0;
+    private BluetoothAdapter bluetoothAdapter;
+    private List<String> deviceList = new ArrayList<>();
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        List<Item> items =new ArrayList<Item>();
-        items.add(new Item("kelvin Mayeye","kelvinmayeye@gmail.com",R.drawable.b));
-        items.add(new Item("Remie Ongala","remiee2002@gmail.com",R.drawable.b));
-        items.add(new Item("Juma Ally","jumaally123@gmail.com",R.drawable.b));
-        items.add(new Item("Samson kalinga","sungura12daat.com",R.drawable.b));
-        items.add(new Item("odama ogiri","ongirioddy120@gmail.com",R.drawable.b));
+       // RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyAdapter(getApplicationContext(),items));
+        // Check if the device supports Bluetooth
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            // Request permission to enable Bluetooth
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+               // startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                // Bluetooth is already enabled
+              //  showPairedDevices();
+
+        }
+    }
 
 
     }
