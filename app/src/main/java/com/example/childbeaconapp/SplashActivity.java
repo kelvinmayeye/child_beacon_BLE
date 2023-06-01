759294a13964f2521f89b2048b9d443c503a8ae1
kelvinmayeye <kevmayeye97@gmail.com>
2023-06-01 13:27:57 +0300

    working add Splash Screen

---
+++ app/src/main/java/com/example/childbeaconapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.childbeaconapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
